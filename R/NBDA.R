64f87040e5f09d246344f58fb6727fd025971ea0
eadinomahony <eadinkerkhoff@gmail.com>
2024-12-12 14:13:36 +0100

    data upload

--- R/NBDA.R
+++ R/NBDA.R
@@ -24,3 +24,19 @@ library(ggpubr)
 #download NBDA package from github:
 #devtools::install_github("whoppitt/NBDA")
 library(NBDA)
+
+################################################################################
+################################################################################
+
+### THE WHOLE STUDY HINGES UPON THREE DATASETS: 1) events.csv; 2) ILV.csv and 3) assoc-final.csv
+### These are all the sightings and each individual's ILVs 
+### (sex information has been gleaned from blow sampling data)
+### 1) Each row is one sighting
+### 2) Each row is one individual
+### 3) assoc-final.csv is a matrix with dyadic SRI values
+
+### NBDA models using NBDA package: https://github.com/whoppitt/NBDA
+
+################################################################################
+################################################################################
+
